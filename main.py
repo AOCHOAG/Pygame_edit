58d3c03227e7acbd3fc15ba4e2fe1edc212c658c
Alex <aochoag@almunos.nebrija.es>
2021-10-08 10:14:06 +0200

    Screen size selector

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ from main_functions import *
 pygame.init()
 
 #call main routine
-size = [1300, 600] # Define size of windows
-main2(size)
+size = [ 1300 , 600] # Define size of windows
+#main2(size)
+ancho = int (input ("Introduce el ancho de la ventana:"))
+alto = int (input ("Introduce el alto de la ventana:"))
+size [0] = ancho
+size [1] = alto
+main2 (size)
